7b828a2d5d9040fa5f7a3f5af42b823d355ff218
DESKTOP-4RS8SOV\sypark <echi0517@naver.com>
2024-01-20 14:27:18 +0900

    접근제어자 필드,메서드 공부

---
+++ java-basic/src/access/b/AccessOuterMain.java
@@ -0,0 +1,27 @@
+package access.b;
+
+import access.a.AccessData;
+
+public class AccessOuterMain {
+
+    public static void main(String[] args) {
+        AccessData data = new AccessData();
+
+
+        // public 패키지
+        data.publicField = 1;
+        data.publicMethod();
+
+
+        //default 패키지
+//        data.defaultField = 2;
+//        data.defaultMethod();
+
+        // private 패키지
+//        data.privateField = 3;
+//        data.privateMethod();
+
+        data.innerAccess();
+
+    }
+}
